9f88378b6ea714660ba1a4a778298098c6a11037
Hoang Nguyen <huahoang2112@gmail.com>
2014-10-03 20:11:54 +0700

    Added all functions for spack mock library

--- src/spark.cc
+++ src/spark.cc
@@ -25,11 +25,11 @@ void Spark_::variable(const char* name, int* p_value){
   gSparkMock->variable(name, p_value);
 }
 
-void Spark_::function(char* funckey, char* funcname){
+void Spark_::function(const char* funckey, const char* funcname){
   gSparkMock->function(funckey, funcname);
 }
 
-void Spark_::subscribe(char* name, char* cbHandler){
+void Spark_::subscribe(const char* name, const char* cbHandler){
   gSparkMock->subscribe(name, cbHandler);
 }
 
@@ -61,12 +61,8 @@ void Spark_::sleep(int seconds){
   gSparkMock->sleep(seconds);
 }
 
-void Spark_::sleep(char* sleep_mode, int seconds){
+void Spark_::sleep(const char* sleep_mode, int seconds){
   gSparkMock->sleep(sleep_mode, seconds);
-}
-
-void Spark_::sleep(uint16_t wakeUpPin, uint16_t edgeTriggerMode){
-  gSparkMock->sleep(wakeUpPin, edgeTriggerMode);
 }
 
 void Spark_::sleep(uint16_t wakeUpPin, uint16_t edgeTriggerMode, int seconds){
